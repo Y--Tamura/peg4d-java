454b73e441d9aa9c4410608d53395d5620cf7729
YTam <yuji.tamura.kt@gmail.com>
2014-11-26 18:05:46 +0900

    added onemore(longest)/oneof/exceptfor with bugs...

--- src/org/peg4d/ext/RegexPegGenerator.java
+++ src/org/peg4d/ext/RegexPegGenerator.java
@@ -2,12 +2,12 @@ package org.peg4d.ext;
 
 
 import org.peg4d.ParsingObject;
-import org.peg4d.ParsingSource;
 
 public class RegexPegGenerator extends Generator {
 
 	private String LF = "\n";
 	private int TerminalCount = 0;
+	private String tmp = null;
 
 	public RegexPegGenerator(String fileName) {
 		super(fileName);
@@ -30,12 +30,17 @@ public class RegexPegGenerator extends Generator {
 //		System.out.println("'"+(char)Integer.parseInt("97",10)+"'");
 	}
 
-//	private void writeTerminalExpression(String expression, String indent, int index){
-//		this.write("e"+index+LF+indent+"= "+expression);
-//	}
+	private void writeTerminalExpression(String indent, int index){
+		this.write(LF+"E"+index+LF+indent+"=");
+	}
+
+	private void writeTerminalExpression(int index){
+		this.writeTerminalExpression("",index);
+	}
 
 	private void parseRegex(ParsingObject pego){
 		String tag = pego.getTag().toString();
+//		System.out.println(tag+" hash:"+pego.hashCode());
 		int i = 0;
 //		System.out.println(tag);
 		if(tag == null){
@@ -45,24 +50,32 @@ public class RegexPegGenerator extends Generator {
 		case "BiginWith":
 		case "EndWith":
 			break;
+		case "onemoreL":
+//			if(next == null){
+//				this.write("+");
+//			}else{
+				this.write(" E"+this.TerminalCount+LF);
+				this.writeTerminalExpression(this.TerminalCount);
+				this.write(" '"+this.tmp+"' E"+this.TerminalCount+" /");
+				this.TerminalCount++;
+//			}
+			break;
 		case "Char":
-			this.write(" '"+pego.getText()+"'");
+			this.tmp = pego.getText();
+			this.write(" '"+this.tmp+"'");
 			break;
 		default:
 			while(i<pego.size()){
-				this.parseRegex(pego.get(i));
+//				if(i+1<pego.size()){
+//					this.parseRegex(pego.get(i),pego.get(i+1));
+//				}else{
+					this.parseRegex(pego.get(i));
+//				}
 				i++;
 			}
 			i = 0;
 			break;
 		}
-	}
-
-	private String makeParsingExpression(ParsingObject pego){
-		int i = 0;
-		ParsingSource source = pego.getSource();
-		String expression = null; //pego.getSource().utf8[i];
-		return expression;
 	}
 
 }
