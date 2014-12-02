cf98c578727b502ae0f8997bef1ac95bbde9d4fd
YTam <yuji.tamura.kt@gmail.com>
2014-12-02 17:27:15 +0900

    enabled 'or' operator

--- src/org/peg4d/regex/RegexObjectConverter.java
+++ src/org/peg4d/regex/RegexObjectConverter.java
@@ -26,27 +26,61 @@ public class RegexObjectConverter {
 	}
 
 	private RegexObject createRegexObject(ParsingObject e) {
-		switch(e.get(1).getTag().toString()) {
-		case "Char":
-		case "OneOf":
-		case "ExceptFor":
-			RegexObject ro = new RegCharSet(e);
-			ro.addQuantifier(e);
-			return ro;
-		case "Block":
-			RegSeq r = new RegSeq();
-			for(ParsingObject blockChild: e.get(1)) {
-				r.add(createRegexObject(blockChild));
+		switch(e.getTag().toString()){
+		case "Or":
+			RegChoice roOr = new RegChoice(null);
+			System.out.println("size :"+e.size());
+			RegSeq r1 = new RegSeq();
+			for(ParsingObject blockChild: e.get(0)) {
+				r1.add(createRegexObject(blockChild));
 			}
-			r.addQuantifier(e);
-			return r;
-		default:
-			System.err.println("Sorry!!!");
-			return null;
+			RegSeq r2 = new RegSeq();
+			for(ParsingObject blockChild: e.get(1)) {
+				r2.add(createRegexObject(blockChild));
+			}
+			roOr.add(r1);
+			roOr.add(r2);
+			return roOr;
+		case "Item":
+			RegSeq roItem = new RegSeq();
+			for(ParsingObject blockChild: e.get(0)) {
+				roItem.add(createRegexObject(blockChild));
+			}
+			return roItem;
+		default: {
+			switch(e.get(1).getTag().toString()) {
+			case "Char":
+			case "OneOf":
+			case "ExceptFor":
+				RegexObject roChar = new RegCharSet(e);
+				roChar.addQuantifier(e);
+				return roChar;
+			case "Block":
+				RegSeq roBlock = new RegSeq();
+				for(ParsingObject blockChild: e.get(1)) {
+					roBlock.add(createRegexObject(blockChild));
+				}
+				roBlock.addQuantifier(e);
+				return roBlock;
+			default:
+				System.err.println("Sorry!!!");
+				return null;
+			}
+		}
 		}
 	}
 
 	private RegexObject pi(RegexObject target, RegexObject continuation) {
-		return null;
+//		if(target == null){
+//			return continuation;
+//		}else if(target.size()==1){
+//			target.add(continuation);
+//			return target;
+//		}else if(target.size()>1){
+//			return null;
+//		}else{
+//			System.err.println("method 'pi' error!!");
+			return null;
+//		}
 	}
 }
